cef9e5533d8a96a6d9ffda2857b5f60b9afb6670
Saritha <saritha451@gmail.com>
2023-08-16 09:03:59 +0400

    Initial commit

---
+++ Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorApp.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? ClassName { get; set; }
+        public string? DivName { get; set; }
+        public StudentModel Clone()
+        {
+            return new StudentModel
+            {
+                Id = this.Id,
+                Name = this.Name,
+                ClassName = this.ClassName,
+                DivName = this.DivName
+            };
+        }
+
+    }
+}
